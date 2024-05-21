0a377c2c947b8a23288ccf63268d051b405b35b0
Sahu, Gaurav <hashcode.dev@gmail.com>
2024-05-21 10:13:16 +0530

    Removing wild card imports

--- src/main/java/com/schoolmate/admissionregistration/model/AdmissionRegistrationDetails.java
+++ src/main/java/com/schoolmate/admissionregistration/model/AdmissionRegistrationDetails.java
@@ -1,6 +1,9 @@
 package com.schoolmate.admissionregistration.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
